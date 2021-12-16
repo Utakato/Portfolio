f0a9965f78d38e3c20a20165ed69dc3d3a550660
Utakato <copyresearch98@gmail.com>
2021-12-16 14:54:51 +0200

    Add Skills component

---
+++ client/src/components/Skills/index.jsx
@@ -0,0 +1,29 @@
+const Skills = () => {
+    return (
+        <section>
+            <h1>Technologies</h1>
+            <div className="wrapper">
+                <div className="card">
+                    <h3>Client-side</h3>
+                    <ul>
+                        <li>React</li>
+                        <li>Redux</li>
+                        <li>HTML5</li>
+                        <li>CSS3</li>
+                    </ul>
+                </div>
+                <div className="card">
+                    <h3>Server-side</h3>
+                    <ul>
+                        <li>Node.js</li>
+                        <li>Express</li>
+                        <li>MongoDB</li>
+                        <li>Python</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Skills
