8c51064763f003ebf26a3b065bc9d0337e3bb885
Utakato <copyresearch98@gmail.com>
2021-12-16 17:49:14 +0200

    Add Projects style

--- client/src/components/Projects/index.jsx
+++ client/src/components/Projects/index.jsx
@@ -1,19 +1,21 @@
+import "./style.css"
 const Projects = () => {
     return (
         <section className="projects">
+            <h1>Projects</h1>
             <div className="wrapper">
                 <div className="project">
-                    <img src="" alt="" />
+                    <img src={`${process.env.PUBLIC_URL}/assets/images/Sapiens.png`} alt="website-preview-image" />
                     </div>
                 <div className="project">
-                    <img src="" alt="" />
+                    <img src={`${process.env.PUBLIC_URL}/assets/images/Moonshots.png`} alt="website-preview-image" />
                     </div>
                 <div className="project">
-                    <img src="" alt="" />
+                    <img src={`${process.env.PUBLIC_URL}/assets/images/Better-me-v2.png`} alt="website-preview-image" />
                     </div>
-                <div className="project">
-                    <img src="" alt="" />
-                    </div>
+                {/* <div className="project">
+                    <img src="" alt="website-preview-image" />
+                    </div> */}
             </div>
         </section>
     )
