d040dc085340d6586ea01ace57866b3d812e6c9a
Utakato <copyresearch98@gmail.com>
2021-12-16 14:59:04 +0200

    Add Projects component

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <Skills />
+      <Projects />
     </div>
   );
 }
---
+++ client/src/components/Projects/index.jsx
@@ -0,0 +1,22 @@
+const Projects = () => {
+    return (
+        <section className="projects">
+            <projects>
+                <div className="project">
+                    <img src="" alt="" />
+                    </div>
+                <div className="project">
+                    <img src="" alt="" />
+                    </div>
+                <div className="project">
+                    <img src="" alt="" />
+                    </div>
+                <div className="project">
+                    <img src="" alt="" />
+                    </div>
+            </projects>
+        </section>
+    )
+}
+
+export default Projects
