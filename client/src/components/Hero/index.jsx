dc89d5e2dbbd8331ec0f531d5935d081b09b2a40
Utakato <copyresearch98@gmail.com>
2021-12-16 17:07:56 +0200

    Add Hero style

--- client/src/components/Hero/index.jsx
+++ client/src/components/Hero/index.jsx
@@ -1,9 +1,12 @@
+import "./style.css"
 const Hero = () => {
     return (
         <div className="hero">
             <h2>Hi, I'm Vasile.</h2>
-            <h1>I'm a web developer.</h1>
-            <h3>*and I'm passionate about building stuff</h3>
+            <h1>I'm a <span>web developer</span>.</h1>
+            <h3>*I'm passionate about building stuff</h3>
+            <h3 id  ="right">**I also have a thirst for knowledge</h3>
+
         </div>
     )
 }
