98ac7996402ec6a7b2b5b224aea4aa31b856cddb
Utakato <copyresearch98@gmail.com>
2021-12-16 14:00:11 +0200

    Initial commit + Header component

---
+++ client/src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+import "./style.css"
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <div className="logo"><span>Schipor</span> Vasile</div>
+            <div className="menu">
+                <div className="item">Skills</div>
+                <div className="item">Portfolio</div>
+                <div className="item">Contact</div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
