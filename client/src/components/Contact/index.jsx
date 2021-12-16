309e031a4faa5339a583bf3a5ff21aa19c397414
Utakato <copyresearch98@gmail.com>
2021-12-16 16:44:27 +0200

    Add Contact component

---
+++ client/src/components/Contact/index.jsx
@@ -0,0 +1,13 @@
+const Contact = () => {
+    return (
+        <contact>
+            Email: My email.
+            Phone number??? Maybe.
+            Location?
+            Try leafjs?
+            Form?
+        </contact>
+    )
+}
+
+export default Contact
