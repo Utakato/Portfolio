2078096f97ef7a737e43c5a468e9c52f3c72dd95
Utakato <copyresearch98@gmail.com>
2022-02-01 17:38:19 +0200

    Change text and animations

--- client/src/components/Hero/index.jsx
+++ client/src/components/Hero/index.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
             <h2>Hi, I'm Vasile.</h2>
             <h1>I'm a <span>web developer</span>.</h1>
             <h3 className="fade">*I'm passionate about building stuff</h3>
-            <h3 className="fade rightH3">**I also have a thirst for knowledge</h3>
+            <h3 className="fade rightH3">**I have a thirst for knowledge</h3>
 
         </div>
     )
