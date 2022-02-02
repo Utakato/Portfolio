c6c1e8ef919380f8b78cb0d577abe3e2a0b086f3
Utakato <copyresearch98@gmail.com>
2022-02-02 16:36:28 +0200

    Make responsive

--- client/src/components/Hero/index.jsx
+++ client/src/components/Hero/index.jsx
@@ -1,18 +1,19 @@
 import "./style.css"
 const Hero = () => {
+    
     setTimeout(() => {
         const left = document.querySelectorAll('h3')
         left.forEach(item => {
             item.classList.add('visible')
         })
     },500)
+
     return (
         <div className="hero">
             <h2>Hi, I'm Vasile.</h2>
             <h1>I'm a <span>web developer</span>.</h1>
             <h3 className="fade">*I'm passionate about building stuff</h3>
             <h3 className="fade rightH3">**I have a thirst for knowledge</h3>
-
         </div>
     )
 }
