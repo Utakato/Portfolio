d62807542619ba7ea55de6926bca899d8ec6d3ef
Utakato <copyresearch98@gmail.com>
2022-02-07 11:32:28 +0200

    Modify text

--- client/src/components/About/index.jsx
+++ client/src/components/About/index.jsx
@@ -6,7 +6,7 @@ const About = () => {
             <h1>About me</h1>
             <h2>*actually explaining why I'm into programming</h2>
             <div className="wrapper">
-                <div className="card">I've had this thirst for knowledge for quite some time now. Ever since I was younger I enjoyed learning new things( like: 3d modelling, 2d animations, Unity game engine ). These days <span>learning new technologies</span> satisfies this thirst.</div>
+                <div className="card">I've had this thirst for knowledge for quite some time now. Ever since I was younger I enjoyed learning new things (like: 3d modelling, 2d animations, Unity game engine). These days <span>learning new technologies</span> satisfies this thirst.</div>
                 <div className="card" id="right">I was always passionate about building stuff. When I was younger I loved building robots and machines with Lego's. Now I'm moving into building products. This passion for building pushes me to be a <span>full-stack developer</span> so I can easily prototype any ideas I might have. </div>
                 <div className="card">When I discovered that programming is about building things, and not just mindlessly writing code, it was a <span>game changer</span> for me. Since then I've been intrested in all areas of programming, from software arhitecture to building and deploying it.</div>
             </div>
--- client/src/components/Projects/projects.js
+++ client/src/components/Projects/projects.js
@@ -6,12 +6,12 @@ export const moonshots = {
     live: "https://test-moonshots.herokuapp.com/",
     frontEndStack : ["React", "Redux"],
     imageName : "Moonshots.png",
-    backEndStack : ["Node.js", "Express", "MongoDB", "Python(scrappers)"],
+    backEndStack : ["Node.js", "Express", "MongoDB", "Python (scrappers)"],
     details: `This is NOT RESPONSIVE. When I built this I thought people should only use this from a PC. (my views have changed since and a mobile version is WIP) \
     \n\nMoonshots is a tool I made that helps me choose crypto projects which are worth researching. My criteria for a good project is based on Product, Marketing and Community. \
     \n\nMoonshots help me narrow down these project based on Community so I only look into projects that have a good community. \
     \n\nI built a python scrapper that gathers data from coinmarketcap.com and regularly updates it. Also the app is also connected to Twitter through their API to get followers data for each project.(this is running daily on a webserver).\
-    \n\nI store data on a MongoDB(Atlas), I created an API to access this data in Node.js (with express and mongoose), and then used React with redux to display it on the web.  I used redux to store data received from the Node.js API and also store the query that goes the API ( offset for pagination and sort order )`,
+    \n\nI store data on a MongoDB (Atlas), I created an API to access this data in Node.js (with express and mongoose), and then used React with redux to display it on the web.  I used redux to store data received from the Node.js API and also store the query that goes the API ( offset for pagination and sort order )`,
 }   
 
 export const betterMeV2 = {
@@ -47,7 +47,7 @@ export const ossoImobiliare = {
     frontEndStack : ["HTML/CSS", "EJS"],
     backEndStack : ["MongoDB","Node.js", "Python(used to fetch data from CRM API to MongoDB)"],
     imageName : "ossoimobiliare.png",
-    details: `This is my first web-dev project, we needed the site remade, we had no more money(paid ~800$ for a broken wordpress site - delivered in 4 months), I had some python knowledge and got the taste of programming so I started learning Web-dev and decided to make a brand new website for our business.\ 
+    details: `This is my first web-dev project, we needed the site remade, we had no more money (paid ~800$ for a broken wordpress site - delivered in 4 months), I had some python knowledge and got the taste of programming so I started learning Web-dev and decided to make a brand new website for our business.\ 
     \n\n\
     On the first tought I wanted to make it from scratch with React and no design. On the second thought we needed this up and running faster than it would've taken me to build it from scratch(because I barely knew React and web-dev). So I bought an HTML template for 25$ and worked with that (created a mongoDB to store data from the CRM API, a script that ran every day to update the DB and then used EJS to display data on the site.)\
     \n\n\
